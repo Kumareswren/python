53bc049df95cffb4b9c217420c1691fd7ec65582
Kumareswren <91511758+Kumareswren@users.noreply.github.com>
2021-11-13 13:36:07 +0800

    Add files via upload

---
+++ Vehicle.py
@@ -0,0 +1,11 @@
+class Vehicle:
+
+    def Vehicle(carname, cartype, carcolour, carprice):
+        return carname, cartype, carcolour, carprice
+
+    vec1 = Vehicle(carname = "vec1", cartype = "convertible", carcolour = "Green", carprice = "RM65000")
+    print(vec1)
+    vec2 = Vehicle("Vec2", "Van", "Red", "RM25000")
+    print(vec2)
+
+
