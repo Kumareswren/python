19495988ec8ea702dbddbff38d4e62098b287867
Kumareswren <91511758+Kumareswren@users.noreply.github.com>
2021-11-13 13:36:46 +0800

    Add files via upload

---
+++ math_test.py
@@ -0,0 +1,9 @@
+def test_greater():
+    mynum = 10
+    assert mynum > 10
+def test_greater_equal():
+    mynum = 10
+    assert mynum >= 10
+def test_less():
+    mynum = 10
+    assert mynum < 20
